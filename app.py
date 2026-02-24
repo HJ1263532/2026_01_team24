2bbf3910e6af7c1baf2024f0b67d78e6772967d9
HJ1263532 <osisland2918@gmail.com>
2026-02-24 10:24:08 +0900

    FEAT todo리스트 추가, 삭제, 조회

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from backend.todolist.todo import todo_bp
+from dotenv import load_dotenv
+import os
 
 print("app.py start")
 
 app = Flask(__name__)
+app.config["SECRET_KEY"]=os.getenv("FLASK_SECRET_KEY")
 app.register_blueprint(todo_bp)
+
+print(app.url_map)
 
 print("before run")
 
--- backend/todolist/todo.py
+++ backend/todolist/todo.py
@@ -1,24 +1,55 @@
 from flask import Blueprint, request, jsonify
 from backend.db import db
 from datetime import datetime
+from bson import ObjectId
 
 todo_bp = Blueprint("todo", __name__)
 
 todos = db.todos
 
-
-@todo_bp.route("/api/todos/test", methods=["POST"])
-def mongo_test():
-#테스트 함수
-    data = request.get_json(silent=True) or {}
-
-    doc = {
-        "title": data.get("title", "test"),
-        "createdAt": datetime.utcnow()
+@todo_bp.route("/api/todos", methods=["POST"])
+def add_todo():
+    data=request.json
+    if not data or "title" not in data:
+        return jsonify({"error": "title is not reqired"}),400
+    
+    doc={
+        # 나중에 로그인 추가 되면 여기 userId추가해야함. "userId": current_user_id로 변경할 것
+        "userId":None,
+        "title": data["title"],
+        "createdAt":datetime.utcnow()
     }
 
-    result = todos.insert_one(doc)
-
+    result=todos.insert_one(doc)
+    
     return jsonify({
-        "inserted_id": str(result.inserted_id)
+        "id": str(result.inserted_id),
+        "title":doc["title"],
+        "createdAt":doc["createdAt"]
+    }),201
+
+@todo_bp.route("/api/todos/<todo_id>", methods=["DELETE"])
+def delete_todo(todo_id):
+
+    result = todos.delete_one({
+        "_id": ObjectId(todo_id)
     })
+
+    if result.deleted_count == 0:
+        return jsonify({"error": "not found"}), 404
+
+    return jsonify({"message": "deleted"})
+
+@todo_bp.route("/api/todos", methods=["GET"])
+def get_todos():
+
+    result = []
+
+    for doc in todos.find().sort("createdAt", 1):
+        result.append({
+            "id": str(doc["_id"]),
+            "title": doc["title"],
+            "createdAt": doc["createdAt"]
+        })
+
+    return jsonify(result)
