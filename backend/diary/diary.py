4ba1ea071b1ab28c657d9020d5eb8a0fa65ab4f7
Lee Eungyo <ialice119@naver.com>
2026-02-25 16:32:09 +0900

    FEAT 일기 수정 기능 구현

--- backend/diary/diary.py
+++ backend/diary/diary.py
@@ -32,6 +32,7 @@ def create_diary():
     doc["updatedAt"]=doc["updatedAt"].isoformat()
     return jsonify(doc), 201
 
+#일기 전체 조회
 @diary_bp.route("/api/diary", methods=["GET"])
 def get_diaries():
     docs=list(diaries.find().sort("createdAt",-1))
@@ -46,7 +47,7 @@ def get_diaries():
         })
     return jsonify(result), 200
 
-
+#일기 단일 조회
 @diary_bp.route("/api/diary/<diary_id>", methods=["GET"])
 def get_diary(diary_id):
     try:
@@ -64,3 +65,49 @@ def get_diary(diary_id):
             "updatedAt":doc["updatedAt"].isoformat()
     }
     return jsonify(result), 200
+
+
+@diary_bp.route("/api/diary/<diary_id>", methods=["PATCH"])
+def update_diary(diary_id):
+    data=request.get_json(silent=True) or {}
+
+    update_fields={}
+    
+    if "title" in data:
+        title=data.get("title")
+        if not title:
+            return jsonify({"error":"제목이 비어있습니다"}), 400
+        update_fields["title"]=title
+
+    if "content" in data:
+        content=data.get("content")
+        if not content:
+            return jsonify({"error":"내용이 비어있습니다"}), 400
+        update_fields["content"]=content
+
+    if not update_fields:
+        return jsonify({"error":"변경된 사항이 없습니다"}), 400
+    
+    update_fields["updatedAt"]=datetime.utcnow()
+
+    try:
+        result=diaries.update_one(
+            {"_id":ObjectId(diary_id)},
+            {"$set":update_fields}
+        )
+    except:
+        return jsonify({"error":"잙못된 id"}), 400
+    
+    if result.matched_count==0:
+        return jsonify({"error":"해당하는 diary를 찾을 수 없습니다"}),404
+    
+    doc=diaries.find_one({"_id":ObjectId(diary_id)})
+
+    response={
+        "_id":str(doc["_id"]),
+        "title":doc["title"],
+        "content":doc["content"],
+        "createdAt":doc["createdAt"].isoformat(),
+        "updatedAt":doc["updatedAt"].isoformat()
+    }
+    return jsonify(response), 200
