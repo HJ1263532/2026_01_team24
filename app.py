f9076387f8cb2132806805dabda8ea4e25a4e9c7
Lee Eungyo <ialice119@naver.com>
2026-02-25 17:56:33 +0900

    FEAT chat테스트함수

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from backend.todolist.todo import todo_bp
 from backend.diary.diary import diary_bp
-
+from backend.chat.chat import chat_bp
 print("app.py start")
 
 app = Flask(__name__)
 app.register_blueprint(todo_bp)
 app.register_blueprint(diary_bp)
+app.register_blueprint(chat_bp)
 
 print("before run")
 
